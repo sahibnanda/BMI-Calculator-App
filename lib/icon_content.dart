0f7e3f9b58bf6a3a093094df36b1073c3037e4d4
Sahib Nanda <70871918+sahibnanda@users.noreply.github.com>
2021-10-15 02:02:06 -0400

    Create icon_content.dart

---
+++ lib/icon_content.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+class IconContents extends StatelessWidget {
+  IconContents({@required this.icon, this.text});
+  final IconData icon;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          icon,
+          size: 80,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          text,
+          style: kLabelTextStyle,
+        ),
+      ],
+    );
+  }
+}
