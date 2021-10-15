b3430544eb4142f905fd0100ca9ba38e8c6e3e05
Sahib Nanda <70871918+sahibnanda@users.noreply.github.com>
2021-10-15 02:00:37 -0400

    Create bottom_button.dart

---
+++ lib/bottom_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+class BottomButton extends StatelessWidget {
+  BottomButton({@required this.onTap, @required this.buttonTitle});
+
+  final Function onTap;
+  final String buttonTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            buttonTitle,
+            style: TextStyle(
+              fontSize: 30.0,
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ),
+        margin: EdgeInsets.all(10.0),
+        width: double.infinity,
+        height: kBottomContainerHeight,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30.0),
+          color: kBottomContainerColor,
+        ),
+      ),
+    );
+  }
+}
